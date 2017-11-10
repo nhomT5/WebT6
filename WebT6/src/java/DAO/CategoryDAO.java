0ce400b380d53d961e1a3d35f16bc110650df82e
badboy6996 <nhockquay19991@gmail.com>
2017-11-11 02:33:46 +0700

    thêm các mục cơ bản cho web

---
+++ WebT6/src/java/DAO/CategoryDAO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import DB.DBConnect;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import Model.Category;
+
+/**
+ *
+ * @author HOANG
+ */
+public class CategoryDAO {
+
+    // get danh sách thể loại
+    public ArrayList<Category> getListCategory() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "SELECT * FROM category";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Category> list = new ArrayList<>();
+        while (rs.next()) {
+            Category category = new Category();
+            category.setCategoryID(rs.getLong("category_id"));
+            category.setCategoryName(rs.getString("category_name"));
+            list.add(category);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        CategoryDAO dao = new CategoryDAO();
+        for (Category ds : dao.getListCategory()) {
+            System.out.println(ds.getCategoryID() + " - " + ds.getCategoryName());
+        }
+    }
+
+}
---
+++ WebT6/web/cart.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : cart
+    Created on : Nov 11, 2017, 2:05:57 AM
+    Author     : HOANG
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
